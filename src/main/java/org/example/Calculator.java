378c0e143abf3c6cae461a13c50a12c8cd12ab48
Kushagra Ahire <kushagraahire55@gmail.com>
2025-10-17 17:48:04 +0530

    - Added code to handle single numbers.

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Calculator {
     public int add(String numbers){
+        if(numbers.isBlank()){
+            return 0;
+        }
 
+        if(numbers.length() == 1){
+            return Integer.parseInt(numbers);
+        }
+
+        throw new UnsupportedOperationException("Only empty and single numbers are allowed");
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,10 +1,24 @@
 import org.example.Calculator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
     private Calculator calculator;
     @BeforeEach
     void setup(){
         calculator = new Calculator();
+    }
+
+    @Test
+    void addEmptyNumber(){
+        assertEquals(0, calculator.add(""));
+    }
+
+    @Test
+    void addSingleNumber(){
+        assertEquals(2, calculator.add("2"));
     }
 }
